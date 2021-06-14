842428c2124f376e24b673c6d893a70a91cca785
“Minh <minhtt21@fe.edu.vn>
2021-06-14 12:04:33 +0700

    change in index.php title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<body>
 		<?php 
 		
-			echo '<p>TEST POSTGRESQL DATABASE FOR LAB 7 </p>'; 
+			echo '<p>ATN POSTGRESQL DATABASE FOR LAB 7 </p>'; 
 			include("config.php");
 			# Connect to DATABASE
 			$pg_conn = pg_connect($conn_string);
